aa92e65e07dcd2c6d4fe8856dfe819bd63ce75b5
elpen84 <elplist@gmail.com>
2019-10-30 14:11:50 -0700

    Initial commit

---
+++ scripts/forecast.js
@@ -0,0 +1,23 @@
+const key = "FFvTdaXnNU7hQMofoF6WAp0L6R2HBfhk";
+
+//get weather//
+const getWeather = async id => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+//get city//
+const getCity = async city => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
